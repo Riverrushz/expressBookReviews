8a43e0a284623d1640a13c09981608035e0757c1
Riverrushz <90576652+Riverrushz@users.noreply.github.com>
2025-07-31 02:56:04 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,10 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({username, password});
 });
 
 // Get the book list available in the shop
@@ -24,21 +26,48 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     
-const author = req.params.author;
+let author = req.params.author;
+
+for (let index = 1; index <= author.length; index++) {
+    if (author.charAt(index) === "_") {
+        author = author.replace("_"," ");
+    }
+}
+
 let book = Object.values(books).filter(book => book.author.toLowerCase() === author.toLowerCase());
+
+
 res.send(JSON.stringify(book)); 
+
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    let title = req.params.title;
+
+    
+    for (let index = 1; index <= title.length; index++) {
+        if (title.charAt(index) === "_") {
+            title = title.replace("_"," ");
+        }
+    }
+
+        
+   
+    let bookDetails = Object.values(books).filter(bookDetails => bookDetails.title.toLowerCase() === title.toLowerCase());
+res.send(JSON.stringify(bookDetails)); 
 });
 
 //  Get book review
+
+
+
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn
+    
+    res.send(JSON.stringify());
 });
 
 module.exports.general = public_users;
